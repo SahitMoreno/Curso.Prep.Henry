09e0f78dc7cfe222f334ded23212af5eb273acc8
Sahit Valdovinos <sahitvaldovinos@gmail.com>
2021-09-16 19:04:09 -0500

    Hice la tarea JS-II

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -166,17 +166,14 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+
 //   if (numero < 2 && numero % numero === 0 && numero % 1 === 0) {
 //     return true;
 //   }
 //   return false;
 //   }
-if (numero < 2){
+if (numero <= 2){
   return false;
-}
-
-if (numero === 2){
-  return true;
 }
 
 for (i = 2; i < numero; i++){
@@ -187,6 +184,8 @@ for (i = 2; i < numero; i++){
 }
 
 }
+
+
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
@@ -222,7 +221,7 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
-  
+
   // for (i = 0 ; i < 8; i++){
   //   numero = numero + 5;
   // }
